7c0e1b224491a77f7eb463325ac42ce11f9cee51
dragon-slayer27 <vivaan.teotia@gmail.com>
2025-05-20 23:26:15 +0530

    implemented Navbar for mobile and desktop and header

---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,16 @@
+import { Header } from "components";
+
+const dashboard = () => {
+  const user = { name: "Vivaan" };
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"}👋`}
+        description="Track activity trends and popular destinations in real time"
+      />
+      Dashboard Page Contents
+    </main>
+  );
+};
+
+export default dashboard;
